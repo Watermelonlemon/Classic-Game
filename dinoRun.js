3ea305476964687a78122a4b6844bdb3f6d0b8f4
Watermelonlemon <sheewon@stanford.edu>
2022-10-25 12:41:36 -0700

    Add score for trex

--- dinoRun.js
+++ dinoRun.js
@@ -1,6 +1,12 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus")
 const alert = document.querySelector(".alert")
+const countJump = document.querySelector(".countJump")
+let count = 0
+let gameOver = false
+let pressedOnce = false
+
+//countJump.innerHTML = count
 
 function jump() {
     if (dino.classList != "jump") {
@@ -18,9 +24,22 @@ let isAlive = setInterval(function() {
 
     if (cactusLeft <60 && cactusLeft > 30 && dinoTop >= 140) {
         alert.innerHTML = "Game Over!" 
+        gameOver = true
     }
 }, 10);
 
+if (pressedOnce) {
+    alert.innerHTML = count
+}
+
+let jumpCount = setInterval(function () {
+    if (!gameOver) {
+        count ++
+    }
+    countJump.innerHTML = count
+}, 1500)
+
 document.addEventListener("keydown", function (event) {
     jump();
+    pressedOnce = true
 });
