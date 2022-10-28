66bccdb4f4986f97200f253dc5c3972ece0aa59a
Watermelonlemon <sheewon@stanford.edu>
2022-10-27 22:55:26 -0700

    change color

--- js/shooting.js
+++ js/shooting.js
@@ -66,7 +66,7 @@ class Enemy {
 
 const x = canvas.width/2
 const y = canvas.height/2
-const player = new Player(x, y, 30, "yellow")
+const player = new Player(x, y, 30, "rgb(300, 300, 350)")
 
 
 // const projectile = new Projectile (
@@ -89,6 +89,7 @@ const player = new Player(x, y, 30, "yellow")
 
 const projectiles = []
 const enemies = []
+const enemyColors = ["rgb(162, 81, 45)", "rgb(180, 182, 188)", "rgb(192, 206, 255)", "rgb(205, 229, 155)", "rgb(230, 159, 154)"]
 
 function spawnEnemies() {
     setInterval(() => {
@@ -104,26 +105,35 @@ function spawnEnemies() {
             x = Math.random() * canvas.width
             y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
 
         const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
         const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
         }
-
-        enemies.push(new Enemy(x, y, radius, color, velocity))
+        
+        randomColor = Math.floor(Math.random() * enemyColors.length)
+        enemies.push(new Enemy(x, y, radius, enemyColors[randomColor], velocity))
     }, 1000)
 }
 
+let animationId
+
 function animate() {
-    requestAnimationFrame(animate)
+    animationId = requestAnimationFrame(animate)
     // projectile.draw()
     // projectile.update()
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach(projectile => {
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+         if (projectile.x + projectile.radius  < 0 
+            || projectile.x - projectile.radius > canvas.width
+            || projectile.y + projectile.radius < 0
+            || projectile.y - projectile.raidus > canvas.height) {
+            projectiles.splice(index, 1)
+         }
     })
 
     enemies.forEach((enemy, idxEnemy) => {
@@ -131,21 +141,25 @@ function animate() {
         projectiles.forEach((projectile, idxPro) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
             if (dist - enemy.radius - projectile.radius < 1)  {
-                //enemies.splice(idxEnemy, 1)
-                if (enemy.radius > projectile.radius * 2) {
-                    enemy.radius = enemy.radius - projectile.radius * 2
-                } else {
-                    enemies.splice(idxEnemy, 1)
-                }
-                projectiles.splice(idxPro, 1)
+                setTimeout(() => {
+                    if (enemy.radius > projectile.radius * 4) {
+                        enemy.radius = enemy.radius - projectile.radius * 2.5
+                    } else {
+                        enemies.splice(idxEnemy, 1)
+                    }
+                    projectiles.splice(idxPro, 1)
+                }, 0)
             }
-            const distToCenter = Math.hypot(player.x - enemy.x, player.y - enemy.y) 
-            if (distToCenter - player.radius - enemy.radius < 1) {
-                alert("Game Over")
-            }
+        const distToCenter = Math.hypot(player.x - enemy.x, player.y - enemy.y) 
+        if (distToCenter - player.radius - enemy.radius < 1) {
+            cancelAnimationFrame(animationId)
+        }
         })
     })
 }
+
+animate()
+spawnEnemies()
 
 window.addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
@@ -155,9 +169,6 @@ window.addEventListener('click', (event) => {
         y: Math.sin(angle)
     }
     projectiles.push(
-        new Projectile(canvas.width / 2, canvas.height / 2, 5, 'black', velocity)
+        new Projectile(canvas.width / 2, canvas.height / 2, 5, "rgb(255, 255, 255)", velocity)
     )
 }) 
-
-animate()
-spawnEnemies()
